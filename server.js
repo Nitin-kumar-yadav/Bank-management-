fbe8ecaebe300b78a164332c0301788017d3f73c
Nitin-kumar-yadav <nitinkumar94555206@gmail.com>
2026-04-07 23:15:33 +0530

    feat: initialize project structure with Express server and MongoDB connection configuration

---
+++ server.js
@@ -0,0 +1,10 @@
+import app from "./src/app.js";
+import dotenv from "dotenv";
+import { connectDB } from "./src/config/db.js";
+
+dotenv.config();
+
+app.listen(process.env.PORT, () => {
+    console.log(`http://localhost:${process.env.PORT}`)
+    connectDB()
+})
