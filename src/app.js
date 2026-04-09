502e9e22bece00534b0be52f8690484034f01786
Nitin-kumar-yadav <nitinkumar94555206@gmail.com>
2026-04-09 22:08:46 +0530

    feat: implement user authentication system with registration and login endpoints using JWT and cookies

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRoutes);
+app.use(cookieParser());
 
 export default app;
 
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,11 +1,89 @@
 import userModel from "../model/user.model.js";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
     try {
         const { email, name, password } = req.body;
+        if (!email || !name || !password) {
+            return res.status(400).json({
+                message: "Email, name and password are required",
+                status: "failed"
+            })
+        }
+        const isExist = await userModel.findOne({
+            email: email
+        })
+        if (isExist) {
+            return res.status(422).json({
+                message: "User already exists",
+                status: "failed"
+            })
+        }
         const user = await userModel.create({ email, name, password });
-        res.status(201).json({ user });
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" })
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "strict",
+            maxAge: 3 * 24 * 60 * 60 * 1000
+        })
+        res.status(201).json({
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email
+            },
+            message: "User registered successfully",
+            status: "success"
+        })
     } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const loginUser = async (req, res) => {
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).json({
+            message: "Email and password are required",
+            status: "failed"
+        })
+    }
+    try {
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email: email }).select("+password");
+        if (!user) {
+            return res.status(401).json({
+                message: "User Not exist",
+                status: "failed"
+            })
+        }
+        const isPasswordValid = await user.comparePassword(password);
+        if (!isPasswordValid) {
+            return res.status(401).json({
+                message: "Invalid Password",
+                status: "failed"
+            })
+        }
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" })
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "strict",
+            maxAge: 3 * 24 * 60 * 60 * 1000
+        })
+        res.status(200).json({
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email
+            },
+            message: "User logged in successfully",
+            status: "success"
+        })
+
+    } catch (error) {
+        console.log(error);
         res.status(500).json({ message: error.message });
     }
 }
