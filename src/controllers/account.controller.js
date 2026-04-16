87cb91db90b6a1f3802d6d40298e2a464d1d08a0
Nitin-kumar-yadav <nitinkumar94555206@gmail.com>
2026-04-16 20:51:10 +0530

    feat: implement account creation controller to handle new user account registration

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -2,18 +2,21 @@ import accountModel from "../model/account.model.js";
 
 const createAccountController = async (req, res) => {
     try {
-        const user = req.user;
-        const account = await accountModel.create({
-            user: user._id,
-            status: "ACTIVE",
-            currency: "INR",
-
-        })
-        return res.status(201).json({ message: "Account created successfully", account });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: "Internal server error" });
+        try {
+            const existingAccount = await accountModel.findOne({ user: user._id });
+            if (existingAccount) {
+                return res.status(409).json({ message: "Account already exists" });
+            }
+            const account = await accountModel.create({
+                user: user._id,
+                status: "ACTIVE",
+                currency: "INR",
+            })
+            return res.status(201).json({ message: "Account created successfully", account });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ message: "Internal server error" });
+        }
     }
-}
 
 export default createAccountController
