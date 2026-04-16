8cc300a38d1d4c5ffc55b190da8c9bdbcd5bf2f8
Nitin-kumar-yadav <nitinkumar94555206@gmail.com>
2026-04-16 20:54:34 +0530

    feat: implement account creation controller for new user bank accounts

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -3,12 +3,12 @@ import accountModel from "../model/account.model.js";
 const createAccountController = async (req, res) => {
     try {
         try {
-            const existingAccount = await accountModel.findOne({ user: user._id });
+            const existingAccount = await accountModel.findOne({ user: req.user._id });
             if (existingAccount) {
                 return res.status(409).json({ message: "Account already exists" });
             }
             const account = await accountModel.create({
-                user: user._id,
+                user: req.user._id,
                 status: "ACTIVE",
                 currency: "INR",
             })
@@ -18,5 +18,10 @@ const createAccountController = async (req, res) => {
             return res.status(500).json({ message: "Internal server error" });
         }
     }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
 
 export default createAccountController
